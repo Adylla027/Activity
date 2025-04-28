3741ff59c2aff6921bdb0bc768146e204cc4fb40
adylla027 <adyllamreiralopesmoreira@gmail.com>
2025-04-28 20:08:24 +0000

    versão pronta

---
+++ app/src/main/java/com/adylla/criandoactivity/MainActivity2.kt
@@ -0,0 +1,29 @@
+package com.adylla.criandoactivity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.adylla.criandoactivity.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar: androidx.appcompat.widget.Toolbar = binding.toolbar
+        toolbar.title = "Tela 2"
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onSupportNavigateUp():Boolean{
+        finish()
+        return true
+    }
+}
